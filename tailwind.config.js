75b220a3b037c6823462c04f6f9ed3dc849a7b65
0xburden <0xburden@protonmail.com>
2023-02-15 16:57:16 -0800

    fix eslint rules, add colors to tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'shiba-yellow': '#EECC78',
+        'shiba-blue': '#2D44B9',
+        'shiba-hot-pink': '#EE097C',
+        'shiba-soft-pink': '#CA9BAD',
+        'shiba-light-green': '#97B294',
+        'shiba-dark-green': '#484E33',
+      },
+    },
   },
   plugins: [],
-};
+}
